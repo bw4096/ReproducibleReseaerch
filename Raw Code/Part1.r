3fd72e2d54d6e0b2c89ae86203d1caae47c175b3
Bryan Willauer <bw4096@yahoo.com>
2020-04-17 10:40:29 -0400

    Added part 2 of the project to the Rmd file

--- Raw Code/Part1.r
+++ Raw Code/Part1.r
@@ -20,3 +20,21 @@ mean_steps_per_day
 
 median_steps_per_day <- steps_per_day %>% summarize(median = median(Steps,na.rm = TRUE))
 median_steps_per_day
+
+# Average steps per time of day
+avg <- df %>% group_by(interval) %>% summarize(Mean = mean(steps, na.rm = TRUE)) 
+avg %>% ggplot(aes(interval, Mean)) +
+    geom_line(col = "Purple") +
+    xlab("Time of Day (24hr time)") +
+    ylab("Average Steps") +
+    ggtitle("Average Number of Steps per Time of Day") +
+    theme(plot.title = element_text(hjust = 0.5))
+
+# Interal with maximum avg steps
+avg$interval[which(avg$Mean == max(avg$Mean))]
+
+
+
+
+
+    
